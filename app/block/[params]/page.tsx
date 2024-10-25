81b06feedab4708b211775cddfa2bc334531adec
rockran <15927686537@139.com>
2024-10-25 11:30:49 +0800

    feat: css image

--- app/block/[params]/page.tsx
+++ app/block/[params]/page.tsx
@@ -1,9 +1,9 @@
-import BlockDetail from "@/components/blockDetail";
 import { queryBlockByNumber, queryBlockByHash } from "@/lib/db";
 import { isHash, hexToDecimal } from "@/lib/utils";
 import Empty from "@/components/empty";
+import style from "./index.module.css";
 
-async function BlockNumber({ params: { params } }: { params: { params: string } }) {
+async function BlockDetail({ params: { params } }: { params: { params: string } }) {
   let data = null;
   if (isHash(params)) {
     data = await queryBlockByHash(params);
@@ -11,9 +11,57 @@ async function BlockNumber({ params: { params } }: { params: { params: string } 
     data = await queryBlockByNumber(hexToDecimal(params));
   }
 
-  if (!data) return <Empty type="block"></Empty>;
+  if (!data) return <Empty></Empty>;
 
-  return <BlockDetail data={data}></BlockDetail>;
+  return (
+    <div className={style.blockDetailPage}>
+      <div className={style.title}>BLOCK</div>
+
+      <div className={`${style.detail} ${style.card}`}>
+        <div className="flex fl_ac">
+          <span>Age</span>
+          <span>{data.timestamp}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Hash</span>
+          <span>{data.hash}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Parent Hash</span>
+          <span>{data.parentHash}</span>
+        </div>
+
+        <div className="flex fl_ac">
+          <span>Total TXs</span>
+          <span>{data.transactionsTotal}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Block Proposer</span>
+          <span>{data.miner}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Gas Limit</span>
+          <span>{data.gasLimit}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Gas Fee</span>
+          <span>{data.gasUsed}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Burnt Fee</span>
+          <span>{data.gasUsed}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Block Rewards</span>
+          <span>{data.gasUsed}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Validators</span>
+          <span>{data.gasUsed}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BlockNumber;
+export default BlockDetail;
--- app/tx/[hash]/page.tsx
+++ app/tx/[hash]/page.tsx
@@ -1,13 +1,45 @@
-import TransactionDetail from "@/components/transactionDetail";
 import { queryTransactionByHash } from "@/lib/db";
 import Empty from "@/components/empty";
+import style from "./index.module.css";
+import type { TxResponseData } from "@/lib/db/type";
 
-async function TransactionHash({ params: { hash } }: { params: { hash: string } }) {
+async function TransactionDetail({ params: { hash } }: { params: { hash: string } }) {
   const data = await queryTransactionByHash(hash);
 
-  if (!data) return <Empty type="block"></Empty>;
+  if (!data) return <Empty></Empty>;
 
-  return <TransactionDetail data={data}></TransactionDetail>;
+  return (
+    <div className={style.txDetailPage}>
+      <div className={style.title}>TRANSACTIONS</div>
+
+      <div className={`${style.detail} ${style.card}`}>
+        <div className="flex fl_ac">
+          <span>TXN Hash</span>
+          <span>{data.hash}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Block</span>
+          <span>{data.blockNumber}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>From-To</span>
+          <span>{data.from}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Age</span>
+          <span>{data.timestamp}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>Amount</span>
+          <span>{data.gas}</span>
+        </div>
+        <div className="flex fl_ac">
+          <span>TXN Fee</span>
+          <span>{data.gasPrice}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TransactionHash;
+export default TransactionDetail;
