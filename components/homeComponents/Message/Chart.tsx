e03421fcf01fc88ad5f113ae6687a5c637cc326e
rockran <15927686537@139.com>
2024-12-05 11:14:35 +0800

    feat: css

--- components/homeComponents/Message/Chart.tsx
+++ components/homeComponents/Message/Chart.tsx
@@ -3,7 +3,14 @@
 import style from "./index.module.css";
 
 function Chart() {
-  return <div className={style.chart}></div>;
+  return (
+    <div className={style.chart}>
+      <iframe
+        style={{ height: "100%", width: "100%", border: "none" }}
+        src="https://dune.com/embeds/4216693/7094693/"
+      />
+    </div>
+  );
 }
 
 export default Chart;
